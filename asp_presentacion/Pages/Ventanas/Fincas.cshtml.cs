917618ea67e1dd1f96b71172b733226542278a94
Sebastian <sebasbedo4@gmail.com>
2025-11-11 10:30:24 -0500

    Hasta la página 32 de la Guía 3.1

---
+++ asp_presentacion/Pages/Ventanas/Fincas.cshtml.cs
@@ -0,0 +1,148 @@
+using lib_dominio.Entidades;
+using lib_dominio.Nucleo;
+using lib_presentaciones.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_presentacion.Pages.Ventanas
+{
+    public class FincasModel : PageModel
+    {
+        private IFincasPresentacion? iPresentacion = null;
+        public FincasModel(IFincasPresentacion iPresentacion)
+        {
+            try
+            {
+                this.iPresentacion = iPresentacion;
+                Filtro = new Fincas { Nombre = string.Empty, Ubicacion = string.Empty };
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+        public IFormFile? FormFile { get; set; }
+        [BindProperty] public Enumerables.Ventanas Accion { get; set; }
+        [BindProperty] public Fincas? Actual { get; set; }
+        [BindProperty] public Fincas? Filtro { get; set; }
+        [BindProperty] public List<Fincas>? Lista { get; set; }
+        public virtual void OnGet() { OnPostBtRefrescar(); }
+        public void OnPostBtRefrescar()
+        {
+            try
+            {
+                var variable_session = HttpContext.Session.GetString("Usuario");
+                if (string.IsNullOrEmpty(variable_session))
+                {
+                    HttpContext.Response.Redirect("/");
+                    return;
+                }
+                Filtro!.Ubicacion = Filtro!.Ubicacion ?? "";
+                Accion = Enumerables.Ventanas.Listas;
+                var task = this.iPresentacion!.PorUbicacion(Filtro!);
+                task.Wait();
+                Lista = task.Result;
+                Actual = null;
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+        public virtual void OnPostBtNuevo()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Editar;
+                Actual = new Fincas { Nombre = string.Empty, Ubicacion = string.Empty};
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+        public virtual void OnPostBtModificar(string data)
+        {
+            try
+            {
+                OnPostBtRefrescar();
+                Accion = Enumerables.Ventanas.Editar;
+                Actual = Lista!.FirstOrDefault(x => x.Id.ToString() == data);
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+        public virtual void OnPostBtGuardar()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Editar;
+                Task<Fincas>? task = null;
+                if (Actual!.Id == 0)
+                    task = this.iPresentacion!.Guardar(Actual!)!;
+                else
+                    task = this.iPresentacion!.Modificar(Actual!)!;
+                task.Wait();
+                Actual = task.Result;
+                Accion = Enumerables.Ventanas.Listas;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+        public virtual void OnPostBtBorrarVal(string data)
+        {
+            try
+            {
+                OnPostBtRefrescar();
+                Accion = Enumerables.Ventanas.Borrar;
+                Actual = Lista!.FirstOrDefault(x => x.Id.ToString() == data);
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+        public virtual void OnPostBtBorrar()
+        {
+            try
+            {
+                var task = this.iPresentacion!.Borrar(Actual!);
+                Actual = task.Result;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+        public void OnPostBtCancelar()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Listas;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+        public void OnPostBtCerrar()
+        {
+            try
+            {
+                if (Accion == Enumerables.Ventanas.Listas)
+                    OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+    }
+}
--- lib_dominio/obj/Debug/net8.0/lib_dominio.AssemblyInfo.cs
+++ lib_dominio/obj/Debug/net8.0/lib_dominio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lib_dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48c9d59c8d773fa75ed6d641620905f56b0bd839")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f247ba3294345ca4cf29a74cf10da7b2f0f90320")]
 [assembly: System.Reflection.AssemblyProductAttribute("lib_dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lib_dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
